e8c7a8ec2ccbfc6cce95f313d5dc5b0dd196a364
RenaDesu <rena-desu@mail.ru>
2022-11-25 20:23:50 +0300

    Добавляет четвертую итерацию реализации второго задания

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,35 +1,18 @@
-// Таймер
-const hours = document.querySelector('.hours');
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds');
-const timerContainer = document.querySelector('.promo__timer-wrapper');
+// Меняет внешний вид списка с сожержанием консультации в мобильной версии
+const productDetails = document.querySelector('.product-details');
+const detailsListTemplate = document.querySelector('#product-list-mobile').content.querySelector('.product-details__list-mobile');
 
-const DEADLINE_DATE = new Date(`December 10 2022 23:59:59`);
+const addDetailsList = () => {
+    const detailsList = detailsListTemplate.cloneNode(true);
+    productDetails.appendChild(detailsList);
+ };
 
+ const removeDetailsList = () => {
+  document.querySelector('.product-details__list-mobile').remove();
+ };
 
-const updateTimer = () => {
-const currentTime = new Date();
-const diff = DEADLINE_DATE - currentTime;
-const hoursLeft = Math.floor(diff / 1000 / 60 / 60);
-const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
-const secondsLeft = Math.floor(diff / 1000) % 60;
-
-hours.innerText = hoursLeft < 10 ? '0' + hoursLeft : hoursLeft;
-minutes.innerText = minutesLeft < 10 ? '0' + minutesLeft : minutesLeft;
-seconds.innerText = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
-
-if (diff <= 0) {
-    clearInterval(timeinterval);
-    timerContainer.innerHTML = 'Акция завершена';
-  }
-};
-
-updateTimer();
-
-const timeinterval = setInterval(updateTimer, 1000);
-
-// Смена порядка ссылок в футере в мобильной версии
-const mainFooterList = document.querySelector('.main-footer__list--mobile');
+ // Смена порядка ссылок в футере в мобильной версии
+ const mainFooterList = document.querySelector('.main-footer__list--mobile');
  const listItemCases = document.querySelector('.main-footer__list-item--cases');
  const listItemLetters = document.querySelector('.main-footer__list-item--letters');
  const listItemCertificates = document.querySelector('.main-footer__list-item--certificates');
@@ -61,10 +44,13 @@ const mainFooterList = document.querySelector('.main-footer__list--mobile');
 
  function onMediaQueryChange(e) {
   if (e.matches) {
+    addDetailsList();
     changeListOrder();
   } else {
+    removeDetailsList();
     setDefaultListOrder();
   }
 }
 
 mediaQueryList.addEventListener('change', onMediaQueryChange);
+
